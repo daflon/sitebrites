ca315094c438c0b53b118de233a6ca02f8c22615
daflon.devops <daflon.devops@alterdata.com.br>
2025-04-07 21:42:58 -0300

    alterando .tsx

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,31 +1,14 @@
 "use client";
 
-export function Card({ children }: { children: React.ReactNode }) {
-  return <div className="border rounded-2xl shadow">{children}</div>;
-}
+import { InputHTMLAttributes } from "react";
 
-export function CardContent({ children }: { children: React.ReactNode }) {
-  return <div className="p-4">{children}</div>;
-}
+type InputProps = InputHTMLAttributes<HTMLInputElement>;
 
-import * as React from "react"
-
-import { cn } from "@/lib/utils"
-
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+export function Input(props: InputProps) {
   return (
     <input
-      type={type}
-      data-slot="input"
-      className={cn(
-        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-        className
-      )}
+      className="border rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-400"
       {...props}
     />
-  )
+  );
 }
-
-export { Input }
